72946d71abc2afa5b1fde241039b1c67922618fc
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-06-11 09:25:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,51 +17,110 @@ let usersSection = document.querySelector('.members__block');
 let videoChatSection = document.querySelector('.video__block');
 let messegesSection = document.querySelector('.messages__block');
 
-let windowWidth = document.documentElement.clientWidth;
-let windowHeight = document.documentElement.clientHeight;
 
-if(windowWidth>700){
-	usersBtn.addEventListener('click', () => {
-		if(messegesSection.classList.contains('disabled')){
-			videoChatSection.classList.toggle('maxVideoWidth');
-		}else if(videoChatSection.classList.contains('disabled') &&
-			messegesSection.classList.contains('maxMessWidth')){
-			messegesSection.classList.remove('maxMessWidth')
-		}else if(videoChatSection.classList.contains('disabled')){
-			messegesSection.classList.add('maxMessWidth');
-		}
-		usersSection.classList.toggle('disabled');
-		messegesSection.classList.toggle('midMessWidth');
-	})
-	videoChatBtn.addEventListener('click', () => {
-		videoChatSection.classList.toggle('disabled');
-		usersSection.classList.toggle('halfWidth');
-		messegesSection.classList.toggle('halfWidth');
-		if(messegesSection.classList.contains('maxMessWidth') || 
-			usersSection.classList.contains('maxMessWidth')){
-			messegesSection.classList.remove('maxMessWidth');
-			usersSection.classList.remove('maxMessWidth');
-		}else if(messegesSection.classList.contains('disabled') || 
-			usersSection.classList.contains('disabled')){
-			messegesSection.classList.add('maxMessWidth');
-			usersSection.classList.add('maxMessWidth');
-		}
-	})
-	messegesBtn.addEventListener('click', () => {
-		if(usersSection.classList.contains('disabled')){
-			videoChatSection.classList.toggle('maxVideoWidth');
-		}else if(videoChatSection.classList.contains('disabled') &&
-			usersSection.classList.contains('maxMessWidth')){
-			usersSection.classList.remove('maxMessWidth')
-		}else if(videoChatSection.classList.contains('disabled')){
-			usersSection.classList.add('maxMessWidth');
-		}
-		messegesSection.classList.toggle('disabled');
-		usersSection.classList.toggle('midMessWidth');
-	})
+
+let phoneBtn = document.querySelector('.phone');
+
+phoneBtn.addEventListener('click', () => {
+	let width = windowWidthChange();
+	console.log(width);
+})
+
+function windowWidthChange () {
+	let windowWidth = document.documentElement.clientWidth;
+	return windowWidth
 }
 
-if(windowWidth<700){
+
+
+
+	usersBtn.addEventListener('click', () => {
+		let width = windowWidthChange();
+		if (width > 700){
+			if(messegesSection.classList.contains('disabled')){
+				videoChatSection.classList.toggle('maxVideoWidth');
+			}else if(videoChatSection.classList.contains('disabled') &&
+				messegesSection.classList.contains('maxMessWidth')){
+				messegesSection.classList.remove('maxMessWidth')
+			}else if(videoChatSection.classList.contains('disabled')){
+				messegesSection.classList.add('maxMessWidth');
+			}
+			usersSection.classList.toggle('disabled');
+			messegesSection.classList.toggle('midMessWidth');
+		}else if(width < 700){
+			if(messegesSection.classList.contains('disabled')){
+				videoChatSection.classList.toggle('maxVideoheight');
+			}else if(videoChatSection.classList.contains('disabled') &&
+				messegesSection.classList.contains('maxMessHeight')){
+				messegesSection.classList.remove('maxMessHeight')
+			}else if(videoChatSection.classList.contains('disabled')){
+				messegesSection.classList.add('maxMessHeight');
+			}
+			usersSection.classList.toggle('disabled');
+			messegesSection.classList.toggle('midMessheight');
+		}
+	})
+
+	videoChatBtn.addEventListener('click', () => {
+		let width = windowWidthChange();
+		if (width > 700){
+			videoChatSection.classList.toggle('disabled');
+			usersSection.classList.toggle('halfWidth');
+			messegesSection.classList.toggle('halfWidth');
+			if(messegesSection.classList.contains('maxMessWidth') || 
+				usersSection.classList.contains('maxMessWidth')){
+				messegesSection.classList.remove('maxMessWidth');
+				usersSection.classList.remove('maxMessWidth');
+			}else if(messegesSection.classList.contains('disabled') || 
+				usersSection.classList.contains('disabled')){
+				messegesSection.classList.add('maxMessWidth');
+				usersSection.classList.add('maxMessWidth');
+			}
+		}else if(width < 700){
+			videoChatSection.classList.toggle('disabled');
+			usersSection.classList.toggle('halfheight');
+			messegesSection.classList.toggle('halfheight');
+			if(messegesSection.classList.contains('maxMessHeight') || 
+				usersSection.classList.contains('maxMessHeight')){
+				messegesSection.classList.remove('maxMessHeight');
+				usersSection.classList.remove('maxMessHeight');
+			}else if(messegesSection.classList.contains('disabled') || 
+				usersSection.classList.contains('disabled')){
+				messegesSection.classList.add('maxMessHeight');
+				usersSection.classList.add('maxMessHeight');
+			}
+		}
+	})
+
+	messegesBtn.addEventListener('click', () => {
+		let width = windowWidthChange();
+		if (width > 700){
+			if(usersSection.classList.contains('disabled')){
+				videoChatSection.classList.toggle('maxVideoWidth');
+			}else if(videoChatSection.classList.contains('disabled') &&
+				usersSection.classList.contains('maxMessWidth')){
+				usersSection.classList.remove('maxMessWidth')
+			}else if(videoChatSection.classList.contains('disabled')){
+				usersSection.classList.add('maxMessWidth');
+			}
+			messegesSection.classList.toggle('disabled');
+			usersSection.classList.toggle('midMessWidth');
+		}else if(width < 700){
+			if(usersSection.classList.contains('disabled')){
+				videoChatSection.classList.toggle('maxVideoheight');
+			}else if(videoChatSection.classList.contains('disabled') &&
+				usersSection.classList.contains('maxMessHeight')){
+				usersSection.classList.remove('maxMessHeight')
+			}else if(videoChatSection.classList.contains('disabled')){
+				usersSection.classList.add('maxMessHeight');
+			}
+			messegesSection.classList.toggle('disabled');
+			usersSection.classList.toggle('midMessheight');
+		}
+	})
+
+
+/*if(windowWidth<700){
 	usersBtn.addEventListener('click', () => {
 		if(messegesSection.classList.contains('disabled')){
 			videoChatSection.classList.toggle('maxVideoheight');
@@ -101,6 +160,8 @@ if(windowWidth<700){
 		usersSection.classList.toggle('midMessheight');
 	})
 }
+*/
+
 
 
 
@@ -114,9 +175,7 @@ let monitor = document.getElementById('monitor');
 
 navButtonsArray.forEach(navBtn => {
 	navBtn.addEventListener('click', () => {
-		let innerSvg = navBtn.querySelector('svg');
 		navBtn.classList.toggle('active');
-		innerSvg.classList.toggle('activeSvg');
 	})
 });
 
@@ -130,22 +189,17 @@ navButtonsArray.forEach(navBtn => {
 userButtonsArray.forEach( btn => {
 	btn.addEventListener('click', () => {
 		let muteItem = btn.querySelector('.mute__span');
-		let muteSvg = btn.querySelector('svg');
 		if (muteItem.classList.contains('mute')){
 			muteItem.classList.remove('mute');
 			btn.classList.add('unmute');
-			muteSvg.classList.add('unmuteSvg');
 		}else if(!muteItem.classList.contains('mute')){
 			muteItem.classList.add('mute');
 			btn.classList.remove('unmute');
-			muteSvg.classList.remove('unmuteSvg');
 		}
 	})	
 });
 
 monitor.addEventListener('click', () => {
-	let muteSvg = monitor.querySelector('svg');
 	monitor.classList.toggle('unmute');
-	muteSvg.classList.toggle('unmuteSvg');
 })
 
